e1894b9e4aa71f97930cc1506006c1ee07e2a806
yana_ratnikova <121186204+yanaratnikova@users.noreply.github.com>
2023-10-25 20:02:37 +0300

    Update TeacherView.java

--- src/main/java/org/example/view/TeacherView.java
+++ src/main/java/org/example/view/TeacherView.java
@@ -1,2 +1,13 @@
-package org.example.view;public class TeacherView {
+package org.example.view;
+
+import org.example.data.Teacher;
+
+import java.util.List;
+
+public class TeacherView {
+    public void displayTeachers(List<Teacher> teachers){
+        for (Teacher teacher : teachers){
+            System.out.println("Name: "+ teacher.getFirstName()+" "+teacher.getSecondName()+" "+teacher.getLastName()+", Subject: "+ teacher.getSubject());
+        }
+    }
 }
