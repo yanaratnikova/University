8e0634a553aab52d7c076d1ced2a783e9c71d37c
yana_ratnikova <121186204+yanaratnikova@users.noreply.github.com>
2023-10-25 19:49:39 +0300

    Update StudentGroupControler.java

--- src/main/java/org/example/controler/StudentGroupControler.java
+++ src/main/java/org/example/controler/StudentGroupControler.java
@@ -1,2 +1,37 @@
-package org.example.controler;public class StudentGroupControler {
+package org.example.controler;
+
+import org.example.data.Student;
+import org.example.service.StudentGroupService;
+import org.example.view.StudentGroupView;
+
+import java.util.List;
+
+public class StudentGroupControler implements UserControler<Student>{
+    StudentGroupService studentGroupService = new StudentGroupService();
+    StudentGroupView studentGroupView = new StudentGroupView();
+
+    public void createStudentGroup(List<Student> studentList){
+        studentGroupService.createGroup(studentList);
+    }
+
+    public List<Student> sortedStudentGroupBySrBall(){
+        return studentGroupService.getSortedStudentGroupBySRBall();
+    }
+
+    public List<Student> sortedStudentGroupByFIO(){
+        return studentGroupService.getSortedStudentGroupByFIO();
+    }
+    public void print(){
+        studentGroupView.printStudentGroup(studentGroupService.getListStudent());
+    }
+
+    public void print(List<Student> list){
+        studentGroupView.printStudentGroup(list);
+    }
+
+    @Override
+    public Student create(String firstName, String secondName, String lastName) {
+        return studentGroupService.createStudent(firstName, secondName, lastName);
+    }
 }
+
