14c3b039045e90804ee1f1ab0c7441ec318c200d
yana_ratnikova <121186204+yanaratnikova@users.noreply.github.com>
2023-10-25 19:58:42 +0300

    Update Teacher.java

--- src/main/java/org/example/data/Teacher.java
+++ src/main/java/org/example/data/Teacher.java
@@ -1,2 +1,35 @@
-package org.example.data;public class Teacher {
+package org.example.data;
+
+public class Teacher extends User{
+    protected int teacherId;
+    protected String subject;
+
+    public Teacher(String firstName, String secondName, String lastName) {
+        super(firstName, secondName, lastName);
+        this.subject = subject;
+        this.teacherId = teacherId;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "teacherId=" + teacherId +
+                '}';
+    }
 }
