55c0116d1bf37a135e92d4921ea795e1b5269596
yana_ratnikova <121186204+yanaratnikova@users.noreply.github.com>
2023-10-25 19:50:52 +0300

    Update UserControler.java

--- src/main/java/org/example/controler/UserControler.java
+++ src/main/java/org/example/controler/UserControler.java
@@ -1,2 +1,9 @@
-package org.example.controler;public class UserControler {
+package org.example.controler;
+
+import org.example.data.User;
+
+public interface UserControler <T extends User> {
+    T create(String firstName, String secondName, String lastName);
 }
+
+
