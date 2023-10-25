8ec0ccd7367a7acac888fbbf88d8fee4a0498c1d
yana_ratnikova <121186204+yanaratnikova@users.noreply.github.com>
2023-10-25 19:51:20 +0300

    Update Student.java

--- src/main/java/org/example/data/Student.java
+++ src/main/java/org/example/data/Student.java
@@ -1,2 +1,47 @@
-package org.example.data;public class Student {
+package org.example.data;
+
+public class Student extends User implements Comparable<Student> {
+
+    private int studentId;
+    private int srBall;
+
+    public Student(String firstName, String secondName, String lastName, int studentId, int srBall) {
+        super(firstName, secondName, lastName);
+        this.studentId = studentId;
+        this.srBall = srBall;
+    }
+
+
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getSrBall() {
+        return srBall;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + super.getFirstName() + '\'' +
+                ", secondName='" + super.getSecondName() + '\'' +
+                ", lastName='" + super.getLastName() + '\'' +
+                ", studentId=" + studentId +
+                ", srBall=" + srBall +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        if (this.srBall == o.getSrBall())
+            return 0;
+        if(this.srBall > o.srBall)
+            return 1;
+        return -1;
+    }
 }
