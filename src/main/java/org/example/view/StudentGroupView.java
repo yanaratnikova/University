6617db0fd6afa377441c58b9dd8f11b155355514
yana_ratnikova <121186204+yanaratnikova@users.noreply.github.com>
2023-10-25 20:01:46 +0300

    Update StudentGroupView.java

--- src/main/java/org/example/view/StudentGroupView.java
+++ src/main/java/org/example/view/StudentGroupView.java
@@ -1,2 +1,13 @@
-package org.example.view;public class StudentGroupView {
+package org.example.view;
+
+import org.example.data.Student;
+
+import java.util.List;
+
+public class StudentGroupView {
+    public void printStudentGroup(List<Student> studentGroup){
+        for (Student st: studentGroup) {
+            System.out.println(st);
+        }
+    }
 }
