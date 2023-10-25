c98f9f4fab9ed3cd5cf1d10c3acb24e249d6dbe5
yana_ratnikova <121186204+yanaratnikova@users.noreply.github.com>
2023-10-25 19:59:38 +0300

    Update UserComaporator.java

--- src/main/java/org/example/data/UserComaporator.java
+++ src/main/java/org/example/data/UserComaporator.java
@@ -1,2 +1,22 @@
-package org.example.data;public class UserComaporator {
+package org.example.data;
+
+//import data.User;
+
+import java.util.Comparator;
+
+public class UserComaporator<T extends User> implements Comparator<T> {
+    @Override
+    public int compare(T o1, T o2) {
+        int resultOfComparing = o1.getSecondName().compareTo(o2.getSecondName());
+        if (resultOfComparing == 0) {
+            resultOfComparing = o1.getFirstName().compareTo(o2.getFirstName());
+            if (resultOfComparing == 0) {
+                return o1.getLastName().compareTo(o2.getLastName());
+            } else {
+                return resultOfComparing;
+            }
+        } else {
+            return resultOfComparing;
+        }
+    }
 }
