6b5f49e5ae5ec12b40e9f506e2a3f09667b7519a
yana_ratnikova <121186204+yanaratnikova@users.noreply.github.com>
2023-10-25 19:50:17 +0300

    Update TeacherController.java

--- src/main/java/org/example/controler/TeacherController.java
+++ src/main/java/org/example/controler/TeacherController.java
@@ -1,2 +1,31 @@
-package org.example.controler;public class TeacherController {
+package org.example.controler;
+
+import org.example.data.Student;
+import org.example.data.Teacher;
+import org.example.service.TeacherService;
+import org.example.view.TeacherView;
+
+import java.util.List;
+
+public class TeacherController implements UserControler<Teacher>{
+    private TeacherService teacherService;
+    private TeacherView teacherView;
+    public TeacherController(){
+        teacherService = new TeacherService();
+        teacherView = new TeacherView();
+    }
+
+
+    @Override
+    public Teacher create(String firstName, String secondName, String lastName) {
+        return teacherService.createTeacher(firstName, secondName, lastName);
+    }
+    public void editTeacher(int teacherId, String firstName, String secondName, String lastName, String subject){
+        teacherService.editTeacher(teacherId, firstName, secondName, lastName, subject);
+    }
+    public void displayAllTeachers(){
+        List<Teacher> teachers = teacherService.getAllTeachers();
+        teacherView.displayTeachers(teachers);
+    }
 }
+
