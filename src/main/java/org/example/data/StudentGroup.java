4a4a0dfced22d76f7613b17d7f241ad6d85fed7d
yana_ratnikova <121186204+yanaratnikova@users.noreply.github.com>
2023-10-25 19:57:49 +0300

    Update StudentGroup.java

--- src/main/java/org/example/data/StudentGroup.java
+++ src/main/java/org/example/data/StudentGroup.java
@@ -1,2 +1,29 @@
-package org.example.data;public class StudentGroup {
+package org.example.data;
+
+import org.example.data.Student;
+import org.example.data.StudentGroupIterator;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class StudentGroup implements Iterable<Student>{
+    private List<Student> studentList;
+
+
+    public StudentGroup(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void addGroup(Student student){
+        studentList.add(student);
+    }
+
+    @Override
+    public Iterator<Student> iterator() {
+        return new StudentGroupIterator(this);
+    }
 }
