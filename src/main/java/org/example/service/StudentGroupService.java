13c31528cb185bacd6b013a4e8f7031f0dc47be0
yana_ratnikova <121186204+yanaratnikova@users.noreply.github.com>
2023-10-25 20:00:32 +0300

    Update StudentGroupService.java

--- src/main/java/org/example/service/StudentGroupService.java
+++ src/main/java/org/example/service/StudentGroupService.java
@@ -1,2 +1,51 @@
-package org.example.service;public class StudentGroupService {
+package org.example.service;
+
+import org.example.data.Student;
+import org.example.data.StudentGroup;
+import org.example.data.UserComaporator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
+public class StudentGroupService {
+    private StudentGroup studentGroup;
+
+    public void createGroup(List<Student> studentList){
+        studentGroup = new StudentGroup(studentList);
+    }
+
+    public StudentGroup getStudentGroup() {
+        return studentGroup;
+    }
+
+    public Student createStudent(String firstName, String secondName, String lastName){
+        if(studentGroup == null)
+            createGroup(new ArrayList<>());
+        int maxid = 0;
+        for(Student student : studentGroup.getStudentList())
+            if (student.getStudentId() > maxid)
+                maxid = student.getStudentId();
+        maxid++;
+        Student student = new Student(firstName, secondName, lastName, maxid, 0);
+        studentGroup.addGroup(student);
+        return student;
+    }
+
+    public List<Student> getSortedStudentGroupBySRBall(){
+        List<Student> studentList = new ArrayList<>(studentGroup.getStudentList());
+        Collections.sort(studentList);
+        return studentList;
+
+    }
+
+    public List<Student> getListStudent(){
+        return studentGroup.getStudentList();
+    }
+
+    public List<Student> getSortedStudentGroupByFIO(){
+        List<Student> studentList = new ArrayList<>(studentGroup.getStudentList());
+        studentList.sort(new UserComaporator());
+        return studentList;
+    }
 }
